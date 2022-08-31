30a4633673a0d0fc305909686cdc11a42e7a8515
墨恒 <ylm200322400@163.com>
2022-08-31 15:10:19 +0800

    全新的编程形式

---
+++ src/main/java/site/moheng/ling/items/SpellRuneItem.java
@@ -0,0 +1,8 @@
+package site.moheng.ling.items;
+import net.minecraft.item.Item;
+
+public class SpellRuneItem extends Item {
+    public SpellRuneItem(Settings settings) {
+		super(settings);
+    }
+}
---
+++ src/main/java/site/moheng/ling/mixin/LivingPlayerMixin.java
@@ -0,0 +1,2 @@
+package site.moheng.ling.mixin;public class LivingPlayerMixin {
+}
